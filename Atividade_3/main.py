d45c38f08e17000d88364553d8dc4971656efbbb
seraphritt <isak369@hotmail.com>
2024-11-22 00:11:19 -0300

    naive bayes now working

--- Atividade_3/main.py
+++ Atividade_3/main.py
@@ -1,4 +1,3 @@
-from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
@@ -9,10 +8,14 @@ import pandas
 
 # lendo csv
 df = pandas.read_csv("data/Dmoz-Computers.csv")
+df = df.sample(frac=1).reset_index(drop=True)
 train_size = int(df.shape[0] * 0.8)
 x_train = df['text'][:train_size]
 x_test = df['text'][train_size:]
-categoriess_on_df = set(df['class'])
+categories_on_df = list(set(df['class']))
+dict_cats = {y: x for x, y in enumerate(categories_on_df)}
+print(dict_cats)
+print(categories_on_df)
 count_vector = CountVectorizer()
 x_train_tf = count_vector.fit_transform(x_train)
 x_test_tf = count_vector.transform(x_test)
@@ -22,11 +25,18 @@ tfidf_t = TfidfTransformer()
 xtrain_tfidf = tfidf_t.fit_transform(x_train_tf)
 xtest_tfidf = tfidf_t.transform(x_test_tf)
 # print(xtrain_tfidf)
-
-mnb = MultinomialNB().fit(xtrain_tfidf, news_train.target)
+v = []
+for each in df['class'][:train_size]:
+    v.append(dict_cats[each])
+mnb = MultinomialNB().fit(xtrain_tfidf, v)
 predicted = mnb.predict(xtest_tfidf)
-
-# print(f"Accuracy: {metrics.accuracy_score(news_test.target, predicted)}")
-# print(confusion_matrix(news_test.target, predicted))
-# print(f1_score(news_test.target, predicted, average='macro'))
-# print(f1_score(news_test.target, predicted, average='micro'))
+print(df['class'][train_size:])
+p = []
+for each in df['class'][train_size:]:
+    p.append(dict_cats[each])
+print(p)
+print(predicted)
+print(f"Accuracy: {metrics.accuracy_score(p, predicted)}")
+print(confusion_matrix(p, predicted))
+print(f1_score(p, predicted, average='macro'))
+print(f1_score(p, predicted, average='micro'))
